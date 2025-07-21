d165461729ec8c5aa539d50abf321bb0a0ea2cfe
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2025-07-21 19:52:00 +0100

    Remove PresetColors due to redundancy with ColorManager.

--- Assets/Scripts/Colors/ColorManager.cs
+++ Assets/Scripts/Colors/ColorManager.cs
@@ -6,21 +6,21 @@ public static class ColorManager
     public static Dictionary<FilteredColors, Color> filteredColors =
         new Dictionary<FilteredColors, Color>
     {
-            { FilteredColors.Red,        PresetColors.Red },
-            { FilteredColors.Green,      PresetColors.Blue },
-            { FilteredColors.Blue,       PresetColors.Green },
-            { FilteredColors.Yellow,     PresetColors.Yellow },
-            { FilteredColors.Pink,       PresetColors.Pink },
-            { FilteredColors.Cyan,       PresetColors.Cyan },
-            { FilteredColors.White,      PresetColors.White },
-            { FilteredColors.Black,      PresetColors.Black },
-            { FilteredColors.Grey,       PresetColors.Grey },
-            { FilteredColors.Orange,     PresetColors.Orange },
-            { FilteredColors.LimeGreen,  PresetColors.LimeGreen },
-            { FilteredColors.Purple,     PresetColors.Purple },
-            { FilteredColors.Brown,      PresetColors.Brown },
-            { FilteredColors.DarkRed,    PresetColors.DarkRed },
-            { FilteredColors.DarkGreen,  PresetColors.DarkGreen },
-            { FilteredColors.DarkBlue,   PresetColors.DarkBlue }
+            { FilteredColors.Red,        new Color(200f, 0f, 0f) },
+            { FilteredColors.Green,      new Color(0f, 200f, 0f) },
+            { FilteredColors.Blue,       new Color(0f, 0f, 200f) },
+            { FilteredColors.Yellow,     new Color(200f, 200f, 0f) },
+            { FilteredColors.Pink,       new Color(200f, 0f, 200f) },
+            { FilteredColors.Cyan,       new Color(0f, 200f, 200f) },
+            { FilteredColors.White,      new Color(200f, 200f, 200f) },
+            { FilteredColors.Black,      new Color(0f, 0f, 0f) },
+            { FilteredColors.Grey,       new Color(100f, 100f, 100f) },
+            { FilteredColors.Orange,     new Color(200f, 100f, 0f) },
+            { FilteredColors.LimeGreen,  new Color(0f, 200f, 100f) },
+            { FilteredColors.Purple,     new Color(100f, 0f, 200f) },
+            { FilteredColors.Brown,      new Color(100f, 100f, 0f) },
+            { FilteredColors.DarkRed,    new Color(100f, 0f, 0f) },
+            { FilteredColors.DarkGreen,  new Color(0f, 100f, 0f) },
+            { FilteredColors.DarkBlue,   new Color(0f, 0f, 100f) }
     };
 }
